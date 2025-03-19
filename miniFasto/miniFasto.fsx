412168b22ecbb056e03016c8af484d36f1101ec1
NikolajK1405 <Nikolajkrarup@hotmail.com>
2025-03-19 20:32:35 +0100

    Debug mode added to mem.c, miniFasto continued

--- miniFasto/miniFasto.fsx
+++ miniFasto/miniFasto.fsx
@@ -1,4 +1,5 @@
-(* Mini Fasto for testing and experimental purposes*)
+(* Mini Fasto for testing and experimental purposes
+   minimal error handling as this will be used carefully *)
 
 (* Simple abstract syntax, subset of AbSyn.fs, only int, array, let and iotas*)
 type Type =
@@ -9,58 +10,46 @@ type Value =
     IntVal   of int
   | ArrayVal of Value list * Type
 
+let valueType = function
+  | (IntVal _)         -> Int
+  | (ArrayVal (_,tp))  -> Array tp
+
 (* Should we have If statements? *)
 type Exp<'T> =
     Constant  of Value
   | ArrayLit  of Exp<'T> list * 'T
-  | Plus  of Exp<'T> * Exp<'T>
-  | Let   of Dec<'T> * Exp<'T>
-  | Index of string * Exp<'T> * 'T
-  | Length of Exp<'T> * 'T
-
-and Dec<'T> =
-    Dec of string * Exp<'T>
+  | Plus      of Exp<'T> * Exp<'T>
+  | Let       of string * Exp<'T> * Exp<'T> // Let "a" = exp in exp
+  | Index     of string * Exp<'T> * 'T
+  | Length    of Exp<'T> * 'T
 
 type TypedExp = Exp<Type>
 
 (* ----------------------------------------------------------------------- *)
 (* Absyn of pseudo-Risc-V *)
-type reg  = RN of int | RS of string
+type reg  = string
 type imm  = int
-type addr = string
 
-type Instruction =
-    LABEL   of addr     (* Define a label (for code or data) *)
-  | COMMENT of string   (* Add a comment in the assembler code *)
+(* Pseudo Risc-v instructions*)
+type PseudoRV =
+    LI  of reg*imm      (* LI(rd,imm):      rd = imm *)
+  | MV of reg*reg       (* MV(rd,rs):       rd = rs *)
 
-  (* Data movement *)
-  | LA   of reg*addr    (* LA($rd,addr):    $rd = addr (label) pseudoinstr] *)
-  | LI   of reg*imm     (* LI($rd,imm):     $rd = imm  [pseudoinstr] *)
-  | MV   of reg*reg     (* MV($rd,$rs):     $rd = $rs  [pseudoinstr] *)
-  // could add: LUI (but subsumed by LI)
+  | ADDI of reg*reg*imm (* ADDI(rd,rs,imm): rd = rs + imm*)
+  | ADD  of reg*reg*reg (* ADD(rd,r1,r2):   rd = r1 + r2 *)
 
-  | LW   of reg*reg*imm (* LW($rd,$rs,imm): $rd = *(int * )($rs + imm) *)
-  | LB   of reg*reg*imm (* LB($rd,$rs,imm): $rd = *(signed char * )($rs + imm) *)
-  | LBU  of reg*reg*imm (* LBU($rd,$rs,imm): $rd = *(unsigned char * )($rs + imm) *)
-  | SW   of reg*reg*imm (* SW($rw,$rm,imm): *(int * )($rm + imm) = $rw *)
-  | SB   of reg*reg*imm (* SB($rb,$rm,imm): *(char * )($rm + imm) = $rb *)
-  // could add: LH, SH, LHU
+  | LW of reg*reg*imm   (* LW(rd,rs,imm):   rd = *(int* )(rs + imm) *)
+  | SW of reg*reg*imm   (* SW(rd,rs,imm):   *(int* )(rs + imm) = rd *)
 
-  (* Arithmetic instructions *)
-  | ADD  of reg*reg*reg (* ADD($rd,$rs,$rt): $rd = $rs + $rt *)
-  | ADDI of reg*reg*imm (* ADDI($rd,$rs,imm): $rd = $rs + imm *)
-  | SUB  of reg*reg*reg (* SUB($rd,$rs,$rt): $rd = $rs - $rt *)
-  | MUL  of reg*reg*reg (* MUL($rd,$rs,$rt): $rd = $rs * $rt *)
-  | DIV  of reg*reg*reg (* DIV($rd,$rs,$rt): $rd = $rd / $rs *)
-  // could add: MULH, MULHSU, MULHU, DIVU, REM, REMU
+  | ALLOC of reg*reg    (* ALLOC(rd,rs):    rd = adress of rs allocated words *)
+  | FREE  of reg        (* FREE(rs):        free allocated memory of rs adress *)
 
-  (* Bitwise operations *)
-  | AND  of reg*reg*reg (* AND($rd,$rs,$rt):  $rd = $rs & $rt *)
-  | ANDI of reg*reg*imm (* ANDI($rd,$rs,imm): $rd = $rs & imm *)
-  | OR   of reg*reg*reg (* OR($rd,$rs,$rt):   $rd = $rs | $rt *)
-  | ORI  of reg*reg*imm (* ORI($rd,$rs,imm):  $rd = $rs | imm *)
-  | XOR  of reg*reg*reg (* XOR($rd,$rs,$rt):  $rd = $rs ^ $rt *)
-  | XORI of reg*reg*imm (* XORI($rd,$rs,imm): $rd = $rs ^ imm *)
+let mutable counter = 0
+let newName name =
+    counter <- counter + 1
+    "x" + string counter + "_" + name
+
+let newReg name = newName name
 
 (* ----------------------------------------------------------------------- *)
 (* Symbol table *)
@@ -78,35 +67,170 @@ let rec lookup n tab =
 
 let bind n i (SymTab stab) = SymTab ((n,i)::stab)
 
-let remove n (SymTab stab) =
-    SymTab (List.filter (fun (x, _) -> x <> n) stab)
+type VarTableI = SymTab<Value> (* Interpretor vartable *)
+type VarTableC = SymTab<reg>   (* Compiler vartable *)
+type VarTableS = SymTab<int>   (* Simulator vartable, our registers *)
 
-type VarTable = SymTab<reg>
+(* ----------------------------------------------------------------------- *)
+(* Interpretor *)
+let rec evalExp (e : TypedExp, vtab : VarTableI) : Value =
+    match e with
+    Constant(v) -> v
+  | ArrayLit(l, t) ->
+        let els = (List.map (fun x -> evalExp(x, vtab)) l)
+        ArrayVal (els, t)
+  | Plus(e1, e2) ->
+        let res1 = evalExp(e1, vtab)
+        let res2 = evalExp(e2, vtab)
+        match (res1, res2) with
+          | (IntVal n1, IntVal n2) -> IntVal (n1+n2)
+          | (_, _) -> failwith "Type mismatch in plus operation"
+  | Let(id, e1, e2) ->
+        let res   = evalExp(e1, vtab)
+        let nvtab = bind id res vtab
+        evalExp(e2, nvtab)
+  | Index(id, e, t) ->
+        let indv = evalExp(e, vtab)
+        let arr = lookup id vtab
+        match (arr, indv) with
+          | (None, _) -> failwith "Non existing array variable"
+          | (Some (ArrayVal(lst, t)), IntVal ind) ->
+                let len = List.length lst
+                if 0 <= ind && ind < len
+                then lst.Item ind
+                else failwith "Array index out of bounds"
+          | (Some m, IntVal _) -> failwith "Indexing into non array"
+          | (_, _) -> failwith "Indexing expression type error"
+  | Length(e, _) ->
+        let arr = evalExp(e, vtab)
+        match arr with
+            | ArrayVal(lst, _) -> IntVal (List.length lst)
+            | _ -> failwith "Length applied to non array"
+
+
+(* ----------------------------------------------------------------------- *)
+(* Interpretor tests *)
+let vtab = empty()
+let e1 = Plus(Constant(IntVal 4), Constant(IntVal 20))
+let e2 = ArrayLit((List.init 5 (fun x -> Constant(IntVal x))), Int)
+let e3 = Let("a", e2, Index("a", Constant(IntVal 3), Int))
+let e4 = Length(e2, Array(Int))
+
+printfn "%A" (evalExp(e1, vtab))
+printfn "%A" (evalExp(e2, vtab))
+printfn "%A" (evalExp(e3, vtab))
+printfn "%A" (evalExp(e4, vtab))
 
 (* ----------------------------------------------------------------------- *)
 (* Code generator *)
+
 let rec compileExp  (e      : TypedExp)
-                    (vtable : VarTable)
+                    (vtable : VarTableC)
                     (place  : reg)
-                  : Instruction list =
+                  : PseudoRV list =
   match e with
   | Constant (IntVal n) ->
-    [LI (place, n)]
+    [ LI (place, n) ]
+
   | Constant (ArrayVal (vs, tp)) ->
+    (* Turn into arraylit to reuse code *)
     let arraylit = ArrayLit(List.map (fun v -> Constant (v)) vs, tp)
-    compileExp arraylit vtable
+    compileExp arraylit vtable place
+
   | ArrayLit (elems, tp) ->
+    let sizeReg = newReg "size"
+    let addrReg = newReg "addr"
+    let tmpReg  = newReg "tmp"
 
+    (* Store size in size_reg, allocate, put addres of first elm in addr reg *)
+    let header = [ LI (sizeReg, List.length elems)
+                 ; ALLOC (place, sizeReg)
+                 ; ADDI (addrReg, place, 4) ]
+    
+    (* Compile each expression from list, store it in addrReg addres, increment addrReg*)
+    let compileElem elmExp =
+        let elmCode = compileExp elmExp vtable tmpReg
+        elmCode @
+        [ SW (tmpReg, addrReg, 0)
+        ; ADDI (addrReg, addrReg, 4) ]
+    
+    let elemsCode = List.collect compileElem elems
+    header @ elemsCode
+  | Plus(e1, e2) ->
+    let t1 = newReg "plus_L"
+    let t2 = newReg "plus_R"
+    let code1 = compileExp e1 vtable t1
+    let code2 = compileExp e2 vtable t2
+    code1 @ code2 @ [ADD (place,t1,t2)]
+  | Let(id, e1, e2) ->
+    let t = newReg ("let_" + id)
+    (* Compile e1 and put result into t *)
+    let code1 = compileExp e vtable t
+    let vtab1 = bind id t vtable
+    (* Compile e2 with the new vtable*)
+    let code2 = compileExp e2 vtab1 place
+    code1 @ code2
 
-(*
-    Constant  of Value
-  | ArrayLit  of Exp<'T> list * 'T
-  | Plus  of Exp<'T> * Exp<'T>
-  | Minus of Exp<'T> * Exp<'T>
-  | Times  of Exp<'T> * Exp<'T>
-  | Divide of Exp<'T> * Exp<'T> 
-  | Let   of Dec<'T> * Exp<'T>
-  | Index of string * Exp<'T> * 'T
-  | Length of Exp<'T> * 'T
-  | Iota   of Exp<'T>
-*)
+  | Index(id, e, t) ->
+    (* Compute index *)
+    let indReg  = newReg "arr_ind"
+    let indCode = compileExp e vtable indReg
+
+    (* Computer pointer to start of array data segment *)
+    let arrReg = newReg "arr_data" // Data pointer
+    let arrBeg = match lookup id vtable with // Array pointer
+                 | None -> failwith (sprintf "Array %s not found" id)
+                 | Some regName -> regName
+    let initCode = [ ADDI (arrReg, arrBeg, 4) ] // arrReg = arrBeg + 4
+
+    (* TODO Check bounds... *)
+
+    (* Since we only have int arrays and 2D arrays no type checking needed
+       just multiply index by 4 for word size, 
+       due to limited RISC-V instructions, we use multiple add instructions *)
+    let loadCode = [ ADD (indReg, indReg, indReg) 
+                   ; ADD (indReg, indReg, indReg) 
+                   ; ADD (arrReg, arrReg, indReg)
+                   ; LW  (place, arrReg, 0) ]
+    
+    indCode @ initCode @ loadCode
+  | Length(e, _) ->
+    (* load word from arr pointer *)
+    let arrAddr = newReg "len_arr"
+    let code1    = compileExp e vtable arrAddr
+    code1 @ [ LW(place, arrAddr, 0) ]
+
+(* ----------------------------------------------------------------------- *)
+(* RISC-V simulator *)
+
+let heap = [| for i in 1..100 -> 0 |] 
+let hp = 0
+
+let lookupReg reg vtab = 
+    let res = lookup reg vtab
+    match res with 
+    | Some x -> x
+    | None -> failwith (sprintf "No such register %s" reg)
+
+(* TODO add pc for future, needed for loops and if statements *)
+let simulateInst (ins : PseudoRV, regs : VarTableS) : VarTableS =
+    printfn "Simulating instruction: %A" ins
+    match ins with
+    | LI (r, i) -> 
+      bind r i regs
+    | MV (rd, rs) -> 
+      let rsVal = lookupReg rs regs
+      bind rd rsVal regs
+    | ADDI (rd, rs, i) ->
+      let rsVal = lookupReg rs regs
+      bind rd (rsVal + i) regs
+    | ADD (rd, r1, r2) ->
+      let r1val = lookupReg r1 regs
+      let r2val = lookupReg r2 regs
+      bind rd (r1val + r2val) regs
+
+    (* TODO: Implement*)
+    | LW (rd, rs, i) -> regs
+    | SW (rd, rs, i) -> regs
+    | ALLOC (rd, rs) -> regs
+    | FREE (rs)      -> regs
